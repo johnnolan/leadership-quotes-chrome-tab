6d9e552b932d3917256bbbe835868d55981c3f84
John Nolan <john.nolan@nolanscafe.co.uk>
2017-03-18 21:31:33 +0000

    Updated SS, list of top 10 websites visited added

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,18 @@ fetch('/data/quotes.json', {
 	var quoteItem = jsonPackage[randomNumber];
 	document.getElementById('quote').innerHTML = '"' + quoteItem.quote + '"<div class="author">- ' + quoteItem.author + '</div>';
 });
+
+
+function buildPopupDom(mostVisitedURLs) {
+	var popupDiv = document.getElementById('most-visited');
+	var ol = popupDiv.appendChild(document.createElement('ol'));
+console.log(mostVisitedURLs);
+	for (var i = 0; i < 10; i++) {
+		var li = ol.appendChild(document.createElement('li'));
+		var a = li.appendChild(document.createElement('a'));
+		a.href = mostVisitedURLs[i].url;
+		a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
+	}
+}
+
+chrome.topSites.get(buildPopupDom);
